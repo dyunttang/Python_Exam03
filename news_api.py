352b86010d926bdbc8c8df1a12777763bc8aa40c
ssar <ssar@nate.com>
2021-09-28 18:28:58 +0900

    Final Output

---
+++ news_api.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+
+def getNewsApi():
+
+    url = "http://localhost:8080/news"
+
+    response = requests.get(url)
+
+    newsDto = response.json()
+
+    if newsDto["code"] == 1:
+        return newsDto["data"]
+    else:
+        print(newsDto["msg"])
